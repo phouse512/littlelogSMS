de4937be6790e55638671c03246a09aa49004976
phouse512 <philiphouse2015@u.northwestern.edu>
2015-09-04 08:30:05 -0500

    friday morning train work: setting up templates

--- sms/views.py
+++ sms/views.py
@@ -13,26 +13,24 @@ from twilio.twiml import Response
 
 def index(request):
 
-    return render(request, "index.html")
+    if request.method == 'GET':
+        return render(request, "index.html")
 
-    # if request.method == 'GET':
-    #     return HttpResponse("this is the screen you should see to register")
-    #
-    # form = SignupAliasForm(request.POST)
-    #
-    # if not form.is_valid():
-    #     return HttpResponse("failure")
-    #
-    # if LittleLogAlias.objects.filter(alias=form.cleaned_data['alias']).exists():
-    #     return HttpResponse("alias already exists, please choose another")
-    #
-    # try:
-    #     new_alias = LittleLogAlias(
-    #         alias=form.cleaned_data['alias'],
-    #         email_secret=form.cleaned_data['email_secret'],
-    #     ).save()
-    # except Exception:
-    #     return HttpResponse("could not create account")
+    form = SignupAliasForm(request.POST)
+
+    if not form.is_valid():
+        return HttpResponse("failure")
+
+    if LittleLogAlias.objects.filter(alias=form.cleaned_data['alias']).exists():
+        return HttpResponse("alias already exists, please choose another")
+
+    try:
+        new_alias = LittleLogAlias(
+            alias=form.cleaned_data['alias'],
+            email_secret=form.cleaned_data['email_secret'],
+        ).save()
+    except Exception:
+        return HttpResponse("could not create account")
 
 
 #
